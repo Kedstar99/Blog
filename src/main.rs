2871d9e3961fe495c0c82d16333181d83934cd37
Krish De Souza <kd913@ic.ac.uk>
2018-10-22 23:34:21 +0100

    Init commit

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,142 @@
-//! A Hello World example application for working with Gotham.
+#![allow(unused_variables)]
+#![cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
 
-extern crate gotham;
-extern crate hyper;
-extern crate mime;
+extern crate actix;
+extern crate actix_web;
+extern crate bytes;
+extern crate env_logger;
+extern crate futures;
 
-use gotham::state::State;
+use bytes::Bytes;
+use futures::sync::mpsc;
+use futures::Stream;
 
-const HELLO_WORLD: &'static str = "Hello World!";
+use actix_web::http::{header, Method, StatusCode};
+use actix_web::middleware::session::{self, RequestSession};
+use actix_web::{
+    error, fs, middleware, pred, server, App, Error, HttpRequest, HttpResponse, Path,
+    Result,
+};
+use futures::future::{result, FutureResult};
+use std::{env, io};
 
-/// Create a `Handler` which is invoked when responding to a `Request`.
-///
-/// How does a function become a `Handler`?.
-/// We've simply implemented the `Handler` trait, for functions that match the signature used here,
-/// within Gotham itself.
-pub fn say_hello(state: State) -> (State, &'static str) {
-    (state, HELLO_WORLD)
+/// favicon handler
+fn favicon(req: &HttpRequest) -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/favicon.ico")?)
 }
 
-/// Start a server and call the `Handler` we've defined above for each `Request` we receive.
-pub fn main() {
-    let addr = "127.0.0.1:7878";
-    println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, || Ok(say_hello))
-}
+/// simple index handler
+fn welcome(req: &HttpRequest) -> Result<HttpResponse> {
+    println!("{:?}", req);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use gotham::test::TestServer;
-    use hyper::StatusCode;
-
-    #[test]
-    fn receive_hello_world_response() {
-        let test_server = TestServer::new(|| Ok(say_hello)).unwrap();
-        let response = test_server
-            .client()
-            .get("http://localhost")
-            .perform()
-            .unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = response.read_body().unwrap();
-        assert_eq!(&body[..], b"Hello World!");
+    // session
+    let mut counter = 1;
+    if let Some(count) = req.session().get::<i32>("counter")? {
+        println!("SESSION value: {}", count);
+        counter = count + 1;
     }
+
+    // set counter to session
+    req.session().set("counter", counter)?;
+
+    // response
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/welcome.html")))
+}
+
+/// 404 handler
+fn p404(req: &HttpRequest) -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
+}
+
+/// async handler
+fn index_async(req: &HttpRequest) -> FutureResult<HttpResponse, Error> {
+    println!("{:?}", req);
+
+    result(Ok(HttpResponse::Ok().content_type("text/html").body(
+        format!("Hello {}!", req.match_info().get("name").unwrap()),
+    )))
+}
+
+/// async body
+fn index_async_body(path: Path<String>) -> HttpResponse {
+    let text = format!("Hello {}!", *path);
+
+    let (tx, rx_body) = mpsc::unbounded();
+    let _ = tx.unbounded_send(Bytes::from(text.as_bytes()));
+
+    HttpResponse::Ok()
+        .streaming(rx_body.map_err(|e| error::ErrorBadRequest("bad request")))
+}
+
+/// handler with path parameters like `/user/{name}/`
+fn with_param(req: &HttpRequest) -> HttpResponse {
+    println!("{:?}", req);
+
+    HttpResponse::Ok()
+        .content_type("text/plain")
+        .body(format!("Hello {}!", req.match_info().get("name").unwrap()))
+}
+
+fn main() {
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+    let sys = actix::System::new("basic-example");
+
+    let addr = server::new(
+        || App::new()
+            // enable logger
+            .middleware(middleware::Logger::default())
+            // cookie session middleware
+            .middleware(session::SessionStorage::new(
+                session::CookieSessionBackend::signed(&[0; 32]).secure(false)
+            ))
+            // register favicon
+            .resource("/favicon", |r| r.f(favicon))
+            // register simple route, handle all methods
+            .resource("/welcome", |r| r.f(welcome))
+            // with path parameters
+            .resource("/user/{name}", |r| r.method(Method::GET).f(with_param))
+            // async handler
+            .resource("/async/{name}", |r| r.method(Method::GET).a(index_async))
+            // async handler
+            .resource("/async-body/{name}", |r| r.method(Method::GET).with(index_async_body))
+            .resource("/test", |r| r.f(|req| {
+                match *req.method() {
+                    Method::GET => HttpResponse::Ok(),
+                    Method::POST => HttpResponse::MethodNotAllowed(),
+                    _ => HttpResponse::NotFound(),
+                }
+            }))
+            .resource("/error", |r| r.f(|req| {
+                error::InternalError::new(
+                    io::Error::new(io::ErrorKind::Other, "test"), StatusCode::INTERNAL_SERVER_ERROR)
+            }))
+            // static files
+            .handler("/static", fs::StaticFiles::new("static").unwrap())
+            // redirect
+            .resource("/", |r| r.method(Method::GET).f(|req| {
+                println!("{:?}", req);
+                HttpResponse::Found()
+                    .header(header::LOCATION, "static/welcome.html")
+                    .finish()
+            }))
+            // default
+            .default_resource(|r| {
+                // 404 for GET request
+                r.method(Method::GET).f(p404);
+
+                // all requests that are not `GET`
+                r.route().filter(pred::Not(pred::Get())).f(
+                    |req| HttpResponse::MethodNotAllowed());
+            }))
+
+        .bind("127.0.0.1:8080").expect("Can not bind to 127.0.0.1:8080")
+        .shutdown_timeout(0)    // <- Set shutdown timeout to 0 seconds (default 60s)
+        .start();
+
+    println!("Starting http server: 127.0.0.1:8080");
+    let _ = sys.run();
 }
